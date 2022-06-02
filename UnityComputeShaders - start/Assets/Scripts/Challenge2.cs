b15f3a9a62ae4e3e680fb9256b8871ded31b649a
Christophe Savard <christophe_savard@hotmail.ca>
2022-06-02 16:59:37 -0400

    Challenge 2

--- UnityComputeShaders - start/Assets/Scripts/Challenge2.cs
+++ UnityComputeShaders - start/Assets/Scripts/Challenge2.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace UnityComputeShaders
 {
@@ -8,28 +7,29 @@ namespace UnityComputeShaders
     {
         private const string KERNEL = "CSMain";
 
-        private static readonly int FillColourID    = Shader.PropertyToID("fillColor");
-        private static readonly int ClearColourID   = Shader.PropertyToID("clearColor");
-        private static readonly int TexResolutionID = Shader.PropertyToID("texResolution");
-        private static readonly int ResultID        = Shader.PropertyToID("Result");
-        private static readonly int MainTexID       = Shader.PropertyToID("_MainTex");
+        private static readonly int FillColourID    = Shader.PropertyToID("fillColour");
+        private static readonly int ClearColourID   = Shader.PropertyToID("clearColour");
+        private static readonly int TexResolutionID = Shader.PropertyToID("textureResolution");
+        private static readonly int ResultID        = Shader.PropertyToID("result");
         private static readonly int TimeID          = Shader.PropertyToID("time");
+        private static readonly int SidesID         = Shader.PropertyToID("sides");
+        private static readonly int MainTexID       = Shader.PropertyToID("_MainTex");
 
         [SerializeField]
         private ComputeShader shader;
-        [SerializeField, FormerlySerializedAs("texResolution")]
+        [SerializeField]
         private int textureResolution = 1024;
-        [FormerlySerializedAs("fillColor"), SerializeField]
+        [SerializeField]
         private Color fillColour = new(1f, 1f, 0f, 1f);
-        [FormerlySerializedAs("clearColor"), SerializeField]
+        [SerializeField]
         private Color clearColour = new(0f, 0f, 0.3f, 1f);
+        [SerializeField, Range(3, 12)]
+        private int sides = 5;
 
         private new Renderer renderer;
         private RenderTexture outputTexture;
-
         private int kernelHandle;
 
-        // Use this for initialization
         private void Start()
         {
             this.outputTexture = new(this.textureResolution, this.textureResolution, 0)
@@ -44,12 +44,19 @@ namespace UnityComputeShaders
             InitShader();
         }
 
+        private void OnDestroy()
+        {
+            this.outputTexture.Release();
+        }
+
+        private void Update()
+        {
+            Dispatch();
+        }
+
         private void InitShader()
         {
             this.kernelHandle = this.shader.FindKernel(KERNEL);
-
-            this.shader.SetVector(FillColourID, this.fillColour);
-            this.shader.SetVector(ClearColourID, this.clearColour);
 
             this.shader.SetInt(TexResolutionID, this.textureResolution);
             this.shader.SetTexture(this.kernelHandle, ResultID, this.outputTexture);
@@ -57,16 +64,15 @@ namespace UnityComputeShaders
             this.renderer.material.SetTexture(MainTexID, this.outputTexture);
         }
 
-        private void DispatchShader(int x, int y)
+        private void Dispatch()
         {
             this.shader.SetFloat(TimeID, Time.time);
-            this.shader.Dispatch(this.kernelHandle, x, y, 1);
-        }
+            this.shader.SetVector(FillColourID, this.fillColour);
+            this.shader.SetVector(ClearColourID, this.clearColour);
+            this.shader.SetInt(SidesID, this.sides);
 
-        private void Update()
-        {
-            DispatchShader(this.textureResolution / 8, this.textureResolution / 8);
+            int count = this.textureResolution / 8;
+            this.shader.Dispatch(this.kernelHandle, count, count, 1);
         }
     }
 }
-
