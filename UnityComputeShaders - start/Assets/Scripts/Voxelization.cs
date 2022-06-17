82e46d61cfe89b237c30c0dad2bcdb4b44a76c9b
Christophe Savard <christophe_savard@hotmail.ca>
2022-06-17 15:47:05 -0400

    Mesh voxelization

--- UnityComputeShaders - start/Assets/Scripts/Voxelization.cs
+++ UnityComputeShaders - start/Assets/Scripts/Voxelization.cs
@@ -2,13 +2,25 @@
 
 namespace UnityComputeShaders
 {
+    [RequireComponent(typeof(VoxelizeMesh))]
     public class Voxelization : MonoBehaviour
     {
-    
-        // Start is called before the first frame update
         private void Start()
         {
-        
+            VoxelizeMesh voxelizer = GetComponent<VoxelizeMesh>();
+            voxelizer.Voxelize();
+
+            // ReSharper disable once LocalVariableHidesMember
+            Transform transform = this.transform;
+            Vector3 scale = new(voxelizer.ParticleSize, voxelizer.ParticleSize, voxelizer.ParticleSize);
+            foreach (Vector3 position in voxelizer.Positions)
+            {
+                Transform particle  = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+                particle.position   = position;
+                particle.localScale = scale;
+                particle.parent     = transform;
+
+            }
         }
     }
 }
--- UnityComputeShaders - start/Assets/Scripts/VoxelizeMesh.cs
+++ UnityComputeShaders - start/Assets/Scripts/VoxelizeMesh.cs
@@ -3,33 +3,76 @@ using UnityEngine;
 
 namespace UnityComputeShaders
 {
-    public class VoxelizeMesh : MonoBehaviour 
+    public class VoxelizeMesh : MonoBehaviour
     {
-        public Mesh meshToVoxelize;
+        [SerializeField]
+        public Mesh mesh;
+        [SerializeField, Range(2, 20)]
         public int yParticleCount = 4;
+        [SerializeField, Range(0, 31)]
         public int layer = 9;
 
-        private float particleSize = 0;
+        public float ParticleSize { get; private set; }
 
-        public float ParticleSize{
-            get{
-                return this.particleSize; 
-            }
-        }
+        public List<Vector3> Positions { get; } = new();
 
-        private List<Vector3> positions = new();
-
-        public List<Vector3> PositionList
+        public void Voxelize()
         {
-            get
+            GameObject temp = new("Temp")
             {
-                return this.positions;
-            }
-        }
+                layer = this.layer
+            };
 
-        public void Voxelize(Mesh mesh)
-        {
-        
+            // ReSharper disable once LocalVariableHidesMember
+            MeshCollider collider = temp.AddComponent<MeshCollider>();
+            collider.sharedMesh   = this.mesh;
+
+            float radius      = this.mesh.bounds.extents.y / this.yParticleCount;
+            this.ParticleSize = radius / 2f;
+
+            Vector3 min              = this.mesh.bounds.min;
+            Vector3 max              = this.mesh.bounds.max;
+            Vector3 count            = this.mesh.bounds.extents / radius;
+            Vector3Int particleCount = new((int)count.x, (int)count.y, (int)count.z);
+            LayerMask mask           = 1 << this.layer;
+
+            if ((particleCount.x % 2) == 0)
+            {
+                min.x += (this.mesh.bounds.extents.x - (particleCount.x * radius));
+            }
+
+            float zOffset = 0f;
+            if (((particleCount.z % 2) == 0))
+            {
+                zOffset += (this.mesh.bounds.extents.z - (particleCount.z * radius));
+            }
+
+            for (float y = min.y + radius; y < max.y; y += this.ParticleSize)
+            {
+                for (float x = min.x; x < max.x; x += this.ParticleSize)
+                {
+                    Vector3 offset = new(x, y, min.z);
+                    Vector3 origin = temp.transform.position + offset;
+
+                    if (!Physics.Raycast(origin, Vector3.forward, out RaycastHit hit, 100f, mask)) continue;
+
+                    Vector3 front = hit.point;
+                    origin.z += max.z * 2f;
+
+                    if (!Physics.Raycast(origin, Vector3.back, out hit, 100f, mask)) continue;
+
+                    Vector3 back = hit.point;
+                    int n = Mathf.CeilToInt(front.z / this.ParticleSize);
+
+                    for (float z = front.z; z < back.z; z += this.ParticleSize)
+                    {
+                        float distance = back.z - z;
+                        if (distance < radius / 2f) break;
+
+                        this.Positions.Add(new(front.x, front.y, z + zOffset));
+                    }
+                }
+            }
         }
     }
 }
