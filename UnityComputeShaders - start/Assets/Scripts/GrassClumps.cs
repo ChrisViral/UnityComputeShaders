cf3a72d4f917d3609f5f0ee2824278410ef80a7b
Christophe Savard <csavard@bhvr.com>
2022-07-07 17:00:13 -0400

    Grass test

--- UnityComputeShaders - start/Assets/Scripts/GrassClumps.cs
+++ UnityComputeShaders - start/Assets/Scripts/GrassClumps.cs
@@ -2,6 +2,7 @@
 
 namespace UnityComputeShaders
 {
+    [RequireComponent(typeof(MeshFilter))]
     public class GrassClumps : MonoBehaviour
     {
         private struct GrassClump
@@ -9,64 +10,102 @@ namespace UnityComputeShaders
             public Vector3 position;
             public float lean;
             public float noise;
-
-            public GrassClump( Vector3 pos)
-            {
-                this.position.x = pos.x;
-                this.position.y = pos.y;
-                this.position.z = pos.z;
-                this.lean = 0;
-                this.noise = Random.Range(0.5f, 1);
-                if (Random.value < 0.5f) this.noise = -this.noise;
-            }
         }
 
-        private int SIZE_GRASS_CLUMP = 5 * sizeof(float);
+        private const int CLUMP_STRIDE    = 5 * sizeof(float);
+        private const int ARGS_STRIDE     = 5 * sizeof(uint);
+        private const string GRASS_KERNEL = "LeanGrass";
 
+        private static readonly int ClumpsBufferID = Shader.PropertyToID("clumps");
+        private static readonly int MaxLeanID      = Shader.PropertyToID("maxLean");
+        private static readonly int TimeID         = Shader.PropertyToID("time");
+        private static readonly int ScaleID        = Shader.PropertyToID("_Scale");
+
+        [SerializeField]
         public Mesh mesh;
+        [SerializeField]
         public Material material;
+        [SerializeField]
         public ComputeShader shader;
-        [Range(0,1)]
+        [SerializeField, Range(0f, 1f)]
         public float density = 0.8f;
-        [Range(0.1f,3)]
+        [SerializeField, Range(0.1f, 3f)]
         public float scale = 0.2f;
-        [Range(10, 45)]
-        public float maxLean = 25;
+        [SerializeField, Range(10f, 45f)]
+        public float maxLean = 25f;
 
         private ComputeBuffer clumpsBuffer;
         private ComputeBuffer argsBuffer;
-        private GrassClump[] clumpsArray;
-        private uint[] argsArray = { 0, 0, 0, 0, 0 };
+        private GrassClump[] clumps;
+        private readonly uint[] args   = new uint[5];
         private Bounds bounds;
-        private int timeID;
         private int groupSize;
-        private int kernelLeanGrass;
+        private int leanGrassHandle;
 
-        // Start is called before the first frame update
         private void Start()
         {
-            this.bounds = new(Vector3.zero, new(30, 30, 30));
+            Random.InitState(new System.Random().Next());
+            this.bounds = new(Vector3.zero, new(30f, 30f, 30f));
             InitShader();
-        }
-
-        private void InitShader()
-        {
-        
-        }
-
-        // Update is called once per frame
-        private void Update()
-        {
-            this.shader.SetFloat(this.timeID, Time.time);
-            this.shader.Dispatch(this.kernelLeanGrass, this.groupSize, 1, 1);
-
-            Graphics.DrawMeshInstancedIndirect(this.mesh, 0, this.material, this.bounds, this.argsBuffer);
         }
 
         private void OnDestroy()
         {
             this.clumpsBuffer?.Release();
             this.argsBuffer?.Release();
+        }
+
+        private void Update()
+        {
+            this.shader.SetFloat(TimeID, Time.time);
+            this.shader.Dispatch(this.leanGrassHandle, this.groupSize, 1, 1);
+
+            Graphics.DrawMeshInstancedIndirect(this.mesh, 0, this.material, this.bounds, this.argsBuffer);
+        }
+
+        private void InitShader()
+        {
+            // ReSharper disable once LocalVariableHidesMember
+            Transform transform = this.transform;
+            MeshFilter filter   = GetComponent<MeshFilter>();
+            Bounds meshBounds   = filter.mesh.bounds;
+            Vector3 size        = transform.localScale * 10f * this.density;
+            Vector2 dimensions  = new(meshBounds.extents.x * size.x, meshBounds.extents.z * size.z);
+            int count           = (int)dimensions.x * (int)dimensions.y;
+
+            this.leanGrassHandle = this.shader.FindKernel(GRASS_KERNEL);
+            this.shader.GetKernelThreadGroupSizes(this.leanGrassHandle, out uint x, out _, out _);
+            this.groupSize = Mathf.CeilToInt(count / (float)x);
+            count = this.groupSize * (int)x;
+
+            this.clumps = new GrassClump[count];
+            for (int i = 0; i < count; i++)
+            {
+                Vector3 position = new(Random.Range(-meshBounds.extents.x, meshBounds.extents.x) + meshBounds.center.x,
+                                       0f,
+                                       Random.Range(-meshBounds.extents.z, meshBounds.extents.z) + meshBounds.center.z);
+                GrassClump clump = new()
+                {
+                    position = transform.TransformPoint(position),
+                    noise    = Random.value
+                };
+                clump.noise    = clump.noise < 0.5f ? clump.noise - 1f: clump.noise;
+                this.clumps[i] = clump;
+            }
+
+            this.clumpsBuffer = new(count, CLUMP_STRIDE);
+            this.clumpsBuffer.SetData(this.clumps);
+
+            this.shader.SetBuffer(this.leanGrassHandle, ClumpsBufferID, this.clumpsBuffer);
+            this.shader.SetFloat(MaxLeanID, this.maxLean * Mathf.Deg2Rad);
+
+            this.material.SetBuffer(ClumpsBufferID, this.clumpsBuffer);
+            this.material.SetFloat(ScaleID, this.scale);
+
+            this.args[0] = this.mesh.GetIndexCount(0);
+            this.args[0] = (uint)count;
+            this.argsBuffer = new(1, ARGS_STRIDE, ComputeBufferType.IndirectArguments);
+            this.argsBuffer.SetData(this.args);
         }
     }
 }
--- UnityComputeShaders - start/Assets/Shaders/GrassClumps.shader
+++ UnityComputeShaders - start/Assets/Shaders/GrassClumps.shader
@@ -2,86 +2,95 @@
 {
     Properties
     {
-        _Color ("Color", Color) = (1,1,1,1)
-        _MainTex ("Albedo (RGB)", 2D) = "white" {}
-        _Glossiness ("Smoothness", Range(0,1)) = 0.5
-        _Metallic ("Metallic", Range(0,1)) = 0.0
+        _Colour("Colour", Color)               = (1, 1, 1, 1)
+        _MainTex("Albedo (RGB)", 2D)           = "white" { }
+        _Glossiness("Smoothness", Range(0, 1)) = 0.5
+        _Metallic("Metallic", Range(0, 1))     = 0
     }
+
     SubShader
     {
-        Tags{ "RenderType"="Opaque" }
-        
-		LOD 200
-		Cull Off
-		
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+        Cull Off
+
         CGPROGRAM
-        // Physically based Standard lighting model, and enable shadows on all light types    
+        // Physically based Standard lighting model, and enable shadows on all light types
         #pragma surface surf Standard vertex:vert addshadow fullforwardshadows
         #pragma instancing_options procedural:setup
 
-        sampler2D _MainTex;
+        #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+        struct GrassClump
+        {
+            float3 position;
+            float lean;
+            float noise;
+        };
+
+        StructuredBuffer<GrassClump> clumps;
+        #endif
 
         struct Input
         {
             float2 uv_MainTex;
         };
 
+        sampler2D _MainTex;
         half _Glossiness;
         half _Metallic;
-        fixed4 _Color;
+        fixed4 _Colour;
         float _Scale;
         float4x4 _Matrix;
         float3 _Position;
 
-        float4x4 create_matrix(float3 pos, float theta){
+        float4x4 getPositionMatrix(float3 position, float theta)
+        {
             float c = cos(theta);
             float s = sin(theta);
             return float4x4(
-                c,-s, 0, pos.x,
-                s, c, 0, pos.y,
-                0, 0, 1, pos.z,
-                0, 0, 0, 1
+                c, -s, 0, position.x,
+                s,  c, 0, position.y,
+                0,  0, 1, position.z,
+                0,  0, 0, 1
             );
-        }
-        
-        #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
-            struct GrassClump
-            {
-                float3 position;
-                float lean;
-                float noise;
-            };
-            StructuredBuffer<GrassClump> clumpsBuffer; 
-        #endif
-
-        void vert(inout appdata_full v, out Input data)
-        {
-            UNITY_INITIALIZE_OUTPUT(Input, data);
-
-            #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
-                
-            #endif
         }
 
         void setup()
         {
             #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
-                
+            GrassClump clump = clumps[unity_InstanceID];
+            _Position = clump.position;
+            _Matrix = getPositionMatrix(clump.position, clump.lean);
             #endif
         }
 
-        void surf (Input IN, inout SurfaceOutputStandard o)
+        void vert(inout appdata_full v, out Input IN)
+        {
+            UNITY_INITIALIZE_OUTPUT(Input, IN);
+
+            #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+            v.vertex.xyz *= _Scale;
+            float4 rotated = mul(_Matrix, v.vertex);
+            v.vertex.xyz += _Position;
+            v.vertex = lerp(v.vertex, rotated, v.texcoord.y);
+            #endif
+        }
+
+        void surf(Input IN, inout SurfaceOutputStandard OUT)
         {
             // Albedo comes from a texture tinted by color
-            fixed4 c = tex2D (_MainTex, IN.uv_MainTex);
-            o.Albedo = c.rgb;
+            fixed4 colour = tex2D(_MainTex, IN.uv_MainTex) * _Colour;
+            OUT.Albedo    = colour.rgb;
+            OUT.Alpha     = colour.a;
+
             // Metallic and smoothness come from slider variables
-            o.Metallic = _Metallic;
-            o.Smoothness = _Glossiness;
-            o.Alpha = c.a;
-            clip(c.a-0.4);
+            OUT.Metallic   = _Metallic;
+            OUT.Smoothness = _Glossiness;
+
+            clip(colour.a - 0.4);
         }
         ENDCG
     }
+
     FallBack "Diffuse"
 }
